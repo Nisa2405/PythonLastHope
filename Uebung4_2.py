e72f68553902f8c224062a9c39d9a7d61e6e9b9d
Nisa2405 <148891490+Nisa2405@users.noreply.github.com>
2025-11-04 11:04:57 +0100

    Uebung4_2.py

---
+++ Uebung4_2.py
@@ -0,0 +1,10 @@
+class Film:
+    def __init__(self, titel="", erscheinungsjahr=0):
+        self.titel = titel
+        self.erscheinungsjahr = erscheinungsjahr
+        self.__genres = set()
+
+#Methoden:
+    def add_genre(self,gerne):
+        self.genre.add(genre)
+
