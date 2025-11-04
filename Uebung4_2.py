8479f27e31ded3645c50c412612bf6982188597d
Nisa2405 <148891490+Nisa2405@users.noreply.github.com>
2025-11-04 14:51:35 +0100

    Add files via upload

--- Uebung4_2.py
+++ Uebung4_2.py
@@ -5,6 +5,19 @@ class Film:
         self.__genres = set()
 
 #Methoden:
-    def add_genre(self,gerne):
-        self.genre.add(genre)
+    def add_genre(self,genre):
+        self.__genres.add(genre)
+
+    def remove_genre(self,genre):
+        self.__genres.remove(genre)
+
+    def genre(self):
+        return set(self.__genres)
+        
+f1= Film("Coraline")
+f1.add_genre("Animation")
+f1.add_genre("Grusel")
+f1.remove_genre("Animation")
+
+print(f1.genre())
 
