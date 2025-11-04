01611409ce8eebdc14f64f49694d262ccb98b817
Nisa2405 <148891490+Nisa2405@users.noreply.github.com>
2025-11-04 10:47:04 +0100

    Uebung4_1.py

--- Uebung4_1.py
+++ Uebung4_1.py
@@ -1,1 +1,13 @@
+class Film:
+    def __init__(self, titel="", erscheinungsjahr=0):
+        self.titel = titel
+        self.erscheinungsjahr = erscheinungsjahr
 
+#Drei Objekte erzeugen
+f1= Film("Nemo",2003)
+f2= Film("Cars",2006)
+f3= Film("Coraline",2009)
+
+print(f1.titel, f1.erscheinungsjahr)
+print(f2.titel, f1.erscheinungsjahr)
+print(f3.titel, f1.erscheinungsjahr)
